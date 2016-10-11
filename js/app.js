d12e45a571ce4a877cd2d194e8a9be3be3a115ce
ColinTravis <colint@cybertravis.us>
2016-10-10 21:10:45 -0500

    Added rudimentary JS to change divs based on time of day

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	var d = new Date();
+	var n = d.getHours();
+	if (n > 19 || n < 6){
+    $('.bedroom_header').toggleClass("night");
+  }
+	  // If time is after 7PM or before 6AM, apply night theme to ‘body’
+
+	else if (n > 16 && n < 19)
+	  // If time is between 4PM – 7PM sunset theme to ‘body’
+	  document.body.className = "sunset";
+	else
+	  // Else use ‘day’ theme
+	  document.body.className = "day";
+});
