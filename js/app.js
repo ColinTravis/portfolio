f01400ca8003d6d3586a4894f48b2c96b92f4f28
Colin Travis <colint@cybertravis.us>
2016-10-12 19:40:06 -0500

    Adding SVG

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
-$(document).ready(function(){
+//==================CHANGE DIV BASED ON TIME OF DAY==================
+$(function(){
 	var d = new Date();
 	var n = d.getHours();
 	if (n > 19 || n < 6){
     $('.bedroom_header').toggleClass("night");
+    $('.bedroom_header').html('<p>Different</p>');
   }
 	  // If time is after 7PM or before 6AM, apply night theme to ‘body’
 
@@ -12,4 +14,28 @@ $(document).ready(function(){
 	else
 	  // Else use ‘day’ theme
 	  document.body.className = "day";
+});//==================END: CHANGE DIV BASED ON TIME OF DAY==================
+
+
+//========================== SCROLLMAGIC =====================================
+
+$(document).ready(function() {
+	//init controller
+var scrollMagicController = new ScrollMagic.Controller();
+var tween1 = TweenMax.to('.block',0.5,{
+	backgroundColor:'red',
+	color:'white'
+});
+
+var scene1 = new ScrollMagic.Scene({
+triggerElement: '.container',
+duration:200
+})
+.setPin('.blockpin',{
+pushFollowers: false
+})
+
+scene1.setTween(tween1)
+scene1.addTo(scrollMagicController)
+scene1.addIndicators();
 });
