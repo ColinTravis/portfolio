0680fe99f2c86843219cfcc20e2fec0396b1cfbe
Colin Travis <colint@cybertravis.us>
2016-10-15 21:14:59 -0500

    Edit particle video, add vectors

--- projects/particles/js/app.js
+++ projects/particles/js/app.js
@@ -1,9 +1,9 @@
 var canvasDiv = document.getElementById('particle-canvas');
 var options = {
-  particleColor: '#fff',
+  particleColor: '#08afc6',
   background: '#000000',
-  interactive: true,
+  interactive: false,
   speed: 'medium',
-  density: 'medium'
+  density: 'heavy'
 };
 var particleCanvas = new ParticleNetwork(canvasDiv, options);
