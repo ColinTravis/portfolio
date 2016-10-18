4ace71a04dab5d8146ea9eb1100280388c55267d
Colin Travis <colint@cybertravis.us>
2016-10-17 19:42:42 -0500

    Fixed Clock refreshing, added new images, fixed theme change time

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ $(function(){
   }
 	  // If time is after 7PM or before 6AM, apply night theme to ‘body’
 
-	else if (n > 16 && n < 19)
+	else if (n > 16 && n < 20)
 	  // If time is between 4PM – 7PM sunset theme to ‘body’
     $('.monitors').toggleClass("sunset");
 		else
